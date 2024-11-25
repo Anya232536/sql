6abdb8ab7ebb9cf229d224870bf26eac783892ee
Aannya <90210670+Anya232536@users.noreply.github.com>
2024-11-25 22:03:00 +0300

    Update Grouping_sorting_часть_1_2.sql

--- Grouping_sorting_часть_1_2.sql
+++ Grouping_sorting_часть_1_2.sql
@@ -2,3 +2,6 @@ SELECT products.category, ROUND(AVG(products.price) ::numeric,2) as avg_price fr
 where products.name LIKE '%Home%' 
       or products.name LIKE '%Hair%'
 GROUP BY category
+
+## ищутся слова Home и Hair в название товаров, таким образом отбираются строки, с которыми производится работа дальше, 
+      затем производится сортировка по категориям для расчета средних значений цены
