22f96bec950147212654e54df3b811b84f061ebe
Aannya <90210670+Anya232536@users.noreply.github.com>
2024-11-25 21:58:00 +0300

    Add files via upload

---
+++ Grouping_sorting_часть_1_1_group..sql
@@ -0,0 +1,14 @@
+select users.city, 
+case 
+when users.age BETWEEN 0 and 20 then 'young'
+when users.age BETWEEN 21 and 49 then 'adult'
+when users.age >= 50 then 'old'
+End as age, 
+count(*) FROM users
+group BY users.city, 
+         CASE 
+             WHEN users.age BETWEEN 0 AND 20 THEN 'young'
+             WHEN users.age BETWEEN 21 AND 49 THEN 'adult'
+             WHEN users.age >= 50 THEN 'old'
+         END
+ORDER BY count(*), users.city;   
