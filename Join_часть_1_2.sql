11ff4722af80079b78bb7cd076bb56f4c728707c
Aannya <90210670+Anya232536@users.noreply.github.com>
2024-11-25 23:29:13 +0300

    Update Join_часть_1_2.sql

--- Join_часть_1_2.sql
+++ Join_часть_1_2.sql
@@ -1,13 +1,13 @@
-SELECT customers_new.name as Customer,
-ROUND(AVG(EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new.order_date, 'YYYY-MM-DD HH24:MI:SS'))))::numeric,1) as day_,
-SUM(orders_new.order_ammount) as sum_ammount, 
-COUNT(orders_new.customer_id)
-  from customers_new JOIN orders_new
-on customers_new.customer_id = orders_new.customer_id
-GROUP BY customers_new.name
-HAVING COUNT(orders_new.customer_id) = (select max(count_s) from (SELECT COUNT(orders_new.customer_id) as count_s FROM orders_new
-                                          GROUP by orders_new.customer_id))
-ORDER BY SUM(orders_new.order_ammount) DESC
+SELECT customers_new_3.name as Customer,
+ROUND(AVG(EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new_3.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new_3.order_date, 'YYYY-MM-DD HH24:MI:SS'))))::numeric,1) as day_,
+SUM(orders_new_3.order_ammount) as sum_ammount, 
+COUNT(orders_new_3.customer_id)
+  from customers_new_3 JOIN orders_new_3
+on customers_new_3.customer_id = orders_new_3.customer_id
+GROUP BY customers_new_3.name
+HAVING COUNT(orders_new_3.customer_id) = (select max(count_s) from (SELECT COUNT(orders_new_3.customer_id) as count_s FROM orders_new_3
+                                          GROUP by orders_new_3.customer_id))
+ORDER BY SUM(orders_new_3.order_ammount) DESC
 
 --Среди клиентов, которые сделали максимальное количество заказов (Having), 
 --находим среднее время между заказом и доставкой, а также общую сумму всех их заказов, затем сортируем по убыванию суммы 
