2cbbef6832e5ce94c61cb5f3dc7b9c331bd69ae5
Aannya <90210670+Anya232536@users.noreply.github.com>
2024-11-25 21:44:10 +0300

    Add files via upload

---
+++ Часть_1_1_2.sql
@@ -0,0 +1,113 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.0 (Debian 17.0-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0 (Debian 17.0-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: demo; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.demo (
+    id integer NOT NULL,
+    name character varying(200) DEFAULT ''::character varying NOT NULL,
+    hint text DEFAULT ''::text NOT NULL
+);
+
+
+--
+-- Name: demo_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.demo_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: demo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.demo_id_seq OWNED BY public.demo.id;
+
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.products (
+    id smallint,
+    name character varying(30),
+    category character varying(20),
+    price real
+);
+
+
+--
+-- Name: sellers; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sellers (
+    seller_id smallint,
+    category character varying(20),
+    date_reg character varying(10),
+    date character varying(10),
+    revenue integer,
+    rating smallint,
+    delivery_days smallint
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id smallint,
+    gender character varying(10),
+    age smallint,
+    education character varying(40),
+    city character varying(20)
+);
+
+
+--
+-- Name: demo id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.demo ALTER COLUMN id SET DEFAULT nextval('public.demo_id_seq'::regclass);
+
+
+--
+-- Name: demo demo_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.demo
+    ADD CONSTRAINT demo_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
