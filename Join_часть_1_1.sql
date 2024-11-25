eaab58caf34a8acee47bd414c73c8aa7f3a92c1b
Aannya <90210670+Anya232536@users.noreply.github.com>
2024-11-25 23:27:41 +0300

    Update Join_часть_1_1.sql

--- Join_часть_1_1.sql
+++ Join_часть_1_1.sql
@@ -1,11 +1,11 @@
-SELECT DISTINCT(customers_new.name) as Customer,
-EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new.order_date, 'YYYY-MM-DD HH24:MI:SS'))) as day_
-from customers_new JOIN orders_new
-on customers_new.customer_id = orders_new.customer_id
-where EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new.order_date, 'YYYY-MM-DD HH24:MI:SS'))) 
-= (SELECT max(EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new.order_date, 'YYYY-MM-DD HH24:MI:SS')))) as day_s
-   from orders_new) 
-ORDER BY customers_new.name 
+SELECT DISTINCT(customers_new_3.name) as Customer,
+EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new_3.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new_3.order_date, 'YYYY-MM-DD HH24:MI:SS'))) as day_
+from customers_new_3 JOIN orders_new_3
+on customers_new_3.customer_id = orders_new_3.customer_id
+where EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new_3.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new_3.order_date, 'YYYY-MM-DD HH24:MI:SS'))) 
+= (SELECT max(EXTRACT(DAY FROM AGE(TO_TIMESTAMP(orders_new_3.shipment_date, 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP(orders_new_3.order_date, 'YYYY-MM-DD HH24:MI:SS')))) as day_s
+   from orders_new_3) 
+ORDER BY customers_new_3.name 
 
 --Находим максимально время ожидания среди всех записей таблицы (10 дней), 
 --затем выводим уникальные имена покупателей, которые ожидали заказ 10 дней
